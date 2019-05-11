bd163c63771e2e314470ce36a251b8e8ab9ce712
Benjamin S. Meyers <ben@lionlogic.org>
2019-05-11 16:51:19 -0400

    Change directory structure. Create apikey.txt

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Remove previous installation
+sudo rm -rf /opt/tvdb/
+
+# Copy executable to /usr/local/bin/
 sudo cp tvdb-dl-nfo.php /usr/local/bin/tvdb-dl-nfo
 sudo chmod +x /usr/local/bin/tvdb-dl-nfo
 
+# Download dependencies
 composer require adrenth/thetvdb2
 
-sudo mkdir /opt/composer/
-sudo mv ./vendor /opt/composer/vendor
-sudo chmod ug+r /opt/composer/vendor
+# Move dependencies to /opt/tvdb/
+sudo mkdir /opt/tvdb/
+sudo mkdir /opt/tvdb/composer/
+sudo mv ./vendor /opt/tvdb/composer/vendor
+sudo chmod ug+r /opt/tvdb/composer/vendor
+
+# Create /opt/tvdb/apikey.txt
+sudo touch /opt/tvdb/apikey.txt
